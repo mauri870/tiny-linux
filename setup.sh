a66ea9f4b97a7782359cd2c3b4b9342517def431
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2026-03-24 11:51:27 -0300

    add gawk to setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt update -yq
 apt install -yq autoconf libncurses-dev xz-utils bzip2 qemu-system-x86 flex bison bc cpio wget curl libelf-dev libssl-dev \
-    syslinux dosfstools gcc strace ltrace manpages-dev make gcc-i686-linux-gnu git file
+    syslinux dosfstools gcc strace ltrace manpages-dev make gcc-i686-linux-gnu git file gawk
